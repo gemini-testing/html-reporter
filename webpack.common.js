e3392e97337317aee23eee7876eeeb6d0e6dc7d2
sipayrt <sipayrt@yandex.ru>
2018-01-15 17:01:04 +0300

    chore: automatically generate html for reports

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,15 @@
 
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 
 const staticPath = path.resolve(__dirname, 'lib', 'static');
 
 module.exports = {
     entry: {
-        bundle: ['./index.js', './styles.css']
+        report: ['./index.js', './styles.css'],
+        gui: ['./gui.js', './styles.css']
     },
     context: staticPath,
     output: {
@@ -38,6 +41,23 @@ module.exports = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin('[name].min.css')
+        new ExtractTextPlugin('[name].min.css'),
+        new HtmlWebpackPlugin({
+            title: 'Gemini report',
+            filename: 'index.html',
+            template: 'template.html',
+            chunks: ['report']
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Gemini gui',
+            filename: 'gui.html',
+            template: 'template.html',
+            chunks: ['gui']
+        }),
+        new HtmlWebpackIncludeAssetsPlugin({
+            files: ['index.html'],
+            assets: ['data.js'],
+            append: false
+        })
     ]
 };
