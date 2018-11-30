9ada15239d704f662aa508cff7011360d0429820
Dfurzikova <dfurzikova@yandex.ru>
2018-11-30 20:23:31 +0300

    fix: add type Tool

--- lib/gui/api/index.ts
+++ lib/gui/api/index.ts
@@ -1,17 +1,24 @@
 'use strict';
 const ApiFacade = require('./facade');
+import * as express from 'express';
+
+export interface Tool {
+    gui: { [key: string]: any }
+}
 
 module.exports = class Api {
-    private _gui: any
-    static create(tool: any) {
+    private _gui: { [key: string]: any }
+
+
+    static create(tool: Tool) {
         return new Api(tool);
     }
 
-    constructor(tool: {gui: any}) {
+    constructor(tool: Tool) {
         this._gui = tool.gui = ApiFacade.create();
     }
 
-    initServer(server: any) {
+    initServer(server: express.Application) {
         this._gui.emit(this._gui.events.SERVER_INIT, server);
     }
 };
