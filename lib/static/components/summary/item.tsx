572e914a805e43e0ac41359ea26167caa176aae1
ilya777grin23 <IRuss23@yandex.ru>
2018-11-30 19:38:52 +0300

    style: fix code-style

--- lib/static/components/summary/item.tsx
+++ lib/static/components/summary/item.tsx
@@ -4,9 +4,9 @@ import React, {Component, Fragment} from 'react';
 import classNames from 'classnames';
 
 interface ISummaryItemProps {
-    label: string,
-    value: number,
-    isFailed?: boolean
+    label: string;
+    value: number;
+    isFailed?: boolean;
 }
 export default class SummaryItem extends Component<ISummaryItemProps> {
 
@@ -25,7 +25,7 @@ export default class SummaryItem extends Component<ISummaryItemProps> {
         return (
             <Fragment>
                 <dt className={className}>{label}</dt>
-                <dd className="summary__value">{value}</dd>
+                <dd className='summary__value'>{value}</dd>
             </Fragment>
         );
     }
