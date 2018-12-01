f937252a3832f3ad6d972bebd309f1d9bbbfbd41
Dfurzikova <dfurzikova@yandex.ru>
2018-12-01 11:33:11 +0300

    refactor: to ts gui.js

--- lib/cli-commands/gui.ts
+++ lib/cli-commands/gui.ts
@@ -3,8 +3,23 @@
 const runGui = require('../gui');
 const Api = require('../gui/api');
 const {GUI: commandName} = require('./');
+interface IpluginConfig {
+    enabled: boolean,
+    path: string,
+    defaultView: string,
+    baseHost: string, 
+    scaleImages: boolean,
+    lazyLoadOffset: number
+}
 
-module.exports = (program, pluginConfig, tool) => {
+interface Itool{
+     [key: string]: any
+}
+interface Iprogram{
+    [key: string]: any
+}
+
+module.exports = (program: Iprogram, pluginConfig: IpluginConfig, tool: Itool) => {
     // must be executed here because it adds `gui` field in `gemini` and `hermione tool`,
     // which is available to other plugins and is an API for interacting with the current plugin
     const guiApi = Api.create(tool);
@@ -17,7 +32,7 @@ module.exports = (program, pluginConfig, tool) => {
         .option('--hostname <hostname>', 'Hostname to launch server on', 'localhost')
         .option('-a, --auto-run', 'auto run immediately')
         .option('-O, --no-open', 'not to open a browser window after starting the server')
-        .action((paths, options) => {
+        .action((paths: string, options: {[key: string]: any}) => {
             runGui({paths, tool, guiApi, configs: {options, program, pluginConfig}});
         });
 };
