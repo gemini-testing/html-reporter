89d4de8d85acf9e52c768b0bc77cd5894be36890
NexisSis <Nexis.sis@gmail.com>
2018-12-03 19:42:13 +0300

    refactor: renamed state-success.jsx to tsx

--- lib/static/components/state/state-success.tsx
+++ lib/static/components/state/state-success.tsx
@@ -1,21 +1,22 @@
 'use strict';
 
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import Screenshot from './screenshot';
 import {isUpdatedStatus} from '../../../common-utils';
 
-export default class StateSuccess extends Component {
-    static propTypes = {
-        status: PropTypes.string.isRequired,
-        expected: PropTypes.string.isRequired
-    }
+interface IStateSuccess {
+    status: string;
+    expected: string;
+
+}
+
+export default class StateSuccess extends Component<IStateSuccess> {
 
     render() {
         const {status, expected} = this.props;
 
         return (
-            <div className="image-box__image">
+            <div className='image-box__image'>
                 <Screenshot noCache={isUpdatedStatus(status)} imagePath={expected}/>
             </div>
         );
