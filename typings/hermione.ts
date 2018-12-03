573bc498c3f836ab304519bb5e1ab57cb97b7660
ilya777grin23 <IRuss23@yandex.ru>
2018-12-04 01:52:54 +0300

    refactor: add typings for hermione

--- typings/hermione.ts
+++ typings/hermione.ts
@@ -1,3 +1,6 @@
+import { IBrowser } from './suite-adapter';
+import Mocha from 'mocha';
+
 export interface IOptions {
     enable?: boolean;
     path?: string;
@@ -7,8 +10,71 @@ export interface IOptions {
     lazyLoadOffset?: number;
 }
 
+type Num = number | null;
+
 export interface IHermione {
-    [key: string]: any;
+    domain: null | string;
+    _events: any;
+    _eventsCount: Num;
+    _maxListeners?: number;
+    _config: {
+        configPath: string;
+        desiredCapabilities: Num;
+        gridUrl: string;
+        baseUrl: string;
+        sessionsPerBrowser: number;
+        testsPerSession: number;
+        retry: number;
+        shouldRetry: Num | string;
+        httpTimeout: number;
+        pageLoadTimeout: Num;
+        sessionRequestTimeout: Num;
+        sessionQuitTimeout: Num;
+        waitTimeout: Num;
+        screenshotOnReject: boolean;
+        screenshotOnRejectTimeout: Num;
+        prepareBrowser: null | any;
+        screenshotPath: null | any;
+        screenshotsDir: string | null;
+        calibrate: boolean;
+        compositeImage: boolean;
+        screenshotMode: string;
+        screenshotDelay: Num;
+        tolerance: Num;
+        antialiasingTolerance: Num;
+        meta: null | any;
+        windowSize: Num;
+        orientation: null | string;
+        resetCursor: boolean;
+        browsers: {
+            [key: string]: IBrowser;
+        };
+        prepareEnvironment: null | string;
+        system: {
+            debug: boolean;
+            mochaOpts: Mocha.MochaOptions;
+            ctx: any;
+            patternsOnReject: any;
+            workers: number;
+            testsPerWorker: number;
+            // hex format
+            diffColor: string;
+            tempDir: string;
+        };
+        plugins: {
+            ['html-reporter/hermione-entry']: {
+                path: string;
+            };
+            [path: string]: any;
+        };
+        sets: {
+            [key: string]: any;
+        };
+    };
+    events: {
+        [key: string]: string;
+    };
+    on(event: string, func: (anything: any) => any): void;
 }
 
 export interface IStats {
