e3094595783b6b698470d0c6ec6427c2edefc196
NexisSis <Nexis.sis@gmail.com>
2018-12-05 18:00:39 +0300

    refactor: specific-test-runner transfer to ts

--- lib/gui/tool-runner-factory/runner/specific-test-runner/hermione.ts
+++ lib/gui/tool-runner-factory/runner/specific-test-runner/hermione.ts
@@ -1,5 +1,7 @@
 'use strict';
 
+import {ISuite} from 'typings/suite-adapter';
+
 const SpecificTestRunner = require('./');
 const {mkFullTitle} = require('../../hermione/utils');
 
@@ -7,7 +9,8 @@ module.exports = class HermioneSpecificTestRunner extends SpecificTestRunner {
     _filter() {
         this._collection.disableAll();
 
-        this._tests.forEach((test) => {
+        this._tests.forEach((test: ISuite) => {
+            console.log(test);
             this._collection.enableTest(mkFullTitle(test), test.browserId);
         });
     }
--- lib/gui/tool-runner-factory/runner/specific-test-runner/index.ts
+++ lib/gui/tool-runner-factory/runner/specific-test-runner/index.ts
@@ -1,15 +1,16 @@
 'use strict';
 
 const Runner = require('../runner');
+import {ISuite} from 'typings/suite-adapter';
 
 module.exports = class SpecificTestRunner extends Runner {
-    constructor(collection, tests) {
+    constructor(collection: any, tests: ISuite) {
         super(collection);
 
         this._tests = tests;
     }
 
-    run(runHandler) {
+    run(runHandler: () => any) {
         this._filter();
 
         return super.run(runHandler);
