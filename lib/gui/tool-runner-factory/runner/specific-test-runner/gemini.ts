999a75b35180fdae90d5f538b530c0e15b08ed50
NexisSis <Nexis.sis@gmail.com>
2018-12-05 17:41:21 +0300

    refactor: gemini transfer to tsx

--- lib/gui/tool-runner-factory/runner/specific-test-runner/gemini.ts
+++ lib/gui/tool-runner-factory/runner/specific-test-runner/gemini.ts
@@ -1,13 +1,15 @@
 'use strict';
 
+import {ISuite} from 'typings/suite-adapter';
+
 const SpecificTestRunner = require('./');
 
 module.exports = class GeminiSpecificTestRunner extends SpecificTestRunner {
     _filter() {
         this._collection.disableAll();
 
-        this._tests.forEach(({suite, state, browserId}) => {
-            const suiteName = suite.path.join(' ');
+        this._tests.forEach(({suite, state, browserId}: {suite: ISuite, state: any, browserId: string}) => {
+            const suiteName = suite.path && suite.path.join(' ');
             this._collection.enable(suiteName, {state: state.name, browser: browserId});
         });
     }
