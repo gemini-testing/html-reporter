3024df89d261ef556c307cea2f229fea4059a651
catwithapple <alexeybegunov@gmail.com>
2019-01-15 21:00:44 +0300

    fix: save images for last retry in skipped test

--- hermione.js
+++ hermione.js
@@ -72,6 +72,10 @@ function prepareImages(hermione, pluginConfig, reportBuilder) {
             queue = queue.then(() => failHandler(testResult));
         });
 
+        hermione.on(hermione.events.TEST_PENDING, (testResult) => {
+            queue = queue.then(() => failHandler(testResult));
+        });
+
         hermione.on(hermione.events.RUNNER_END, () => queue.then(resolve, reject));
     });
 }
