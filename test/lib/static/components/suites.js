d95dc16479491ecc7accd18e1c46cc94ddacb18b
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2019-03-20 12:32:36 +0300

    feat: add lazy suites render

---
+++ test/lib/static/components/suites.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import LazilyRender from 'react-lazily-render';
+import proxyquire from 'proxyquire';
+import {defaultsDeep} from 'lodash';
+
+import {mkConnectedComponent} from './utils';
+import {config} from 'lib/constants/defaults';
+
+describe('<Suites/>', () => {
+    let Suites;
+    const SectionCommonStub = class SectionCommonStub extends Component {
+        render() {
+            return <div></div>;
+        }
+    };
+
+    const mkSuitesComponent = (initialState = {}) => {
+        initialState = defaultsDeep(initialState, {
+            gui: false,
+            suiteIds: {all: ['suite1']},
+            view: {viewMode: 'all', filteredBrowsers: [], lazyLoadOffset: config.lazyLoadOffset}
+        });
+
+        return mkConnectedComponent(<Suites />, {initialState});
+    };
+
+    beforeEach(() => {
+        Suites = proxyquire('lib/static/components/suites', {
+            './section/section-common': {
+                default: SectionCommonStub
+            }
+        }).default;
+    });
+
+    it('should wrap suite with Lazy-renderer component by default', () => {
+        const suitesComponent = mkSuitesComponent();
+
+        assert.lengthOf(suitesComponent.find(LazilyRender), 1);
+    });
+
+    it('should pass to Lazy-renderer component "lazyLoadOffset" option', () => {
+        const suitesComponent = mkSuitesComponent({view: {lazyLoadOffset: 100}});
+        const lazyRendererProps = suitesComponent.find(LazilyRender).props();
+
+        assert.equal(lazyRendererProps.offset, 100);
+    });
+
+    it('should not wrap suite with Lazy-renderer component if lazyLoadOffset was disabled', () => {
+        const suitesComponent = mkSuitesComponent({view: {lazyLoadOffset: 0}});
+
+        assert.lengthOf(suitesComponent.find(LazilyRender), 0);
+    });
+});
