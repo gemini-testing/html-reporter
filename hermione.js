0338b80e9394178ee6c066b1e8d888b71c54688f
pakhomova-vm <pakhomova-vm@yandex-team.ru>
2019-09-16 14:05:38 +0300

    made ReportBuilderSqlite a self-sufficient reporter

--- hermione.js
+++ hermione.js
@@ -25,7 +25,7 @@ module.exports = (hermione, opts) => {
     });
 };
 
-async function prepare(hermione, reportBuilder, pluginConfig) {
+async function prepare(hermione, {reportBuilder, reportBuilderSqlite}, pluginConfig) {
     const {path: reportPath} = pluginConfig;
 
     const failHandler = async (testResult) => {
@@ -40,6 +40,19 @@ async function prepare(hermione, reportBuilder, pluginConfig) {
 
         return formattedResult;
     };
+    const failHandlerSqlite = async (testResult) => {
+        const formattedResult = reportBuilderSqlite.format(testResult);
+        const actions = [formattedResult.saveTestImages(reportPath, workers)];
+
+        if (formattedResult.screenshot) {
+            actions.push(formattedResult.saveBase64Screenshot(reportPath));
+        }
+
+        await Promise.all(actions);
+
+        return formattedResult;
+    };
+
 
     const addFail = (formattedResult) => {
         return formattedResult.hasDiff()
@@ -47,35 +60,73 @@ async function prepare(hermione, reportBuilder, pluginConfig) {
             : reportBuilder.addError(formattedResult);
     };
 
+    const addFailSqlite = (formattedResult) => {
+        return formattedResult.hasDiff()
+            ? reportBuilderSqlite.addFail(formattedResult)
+            : reportBuilderSqlite.addError(formattedResult);
+    };
+
     return new Promise((resolve, reject) => {
         const queue = new PQueue({concurrency: os.cpus().length});
         const promises = [];
 
         hermione.on(hermione.events.TEST_PASS, testResult => {
+            console.log("hermione.js test pass");
             promises.push(queue.add(async () => {
-                const formattedResult = reportBuilder.format(testResult);
-                await formattedResult.saveTestImages(reportPath, workers);
 
+                const formattedResult = reportBuilder.format(testResult);
+                // console.log("formattedResult");
+                // console.log(formattedResult);
+                await formattedResult.saveTestImages(reportPath, workers);
                 return reportBuilder.addSuccess(formattedResult);
             }).catch(reject));
+
+            if (reportBuilderSqlite) {
+                promises.push(queue.add(async () => {
+                    const formattedResultSqlite = reportBuilderSqlite.format(testResult);
+                    // console.log("formattedResultSqlite");
+                    // console.log(formattedResultSqlite);
+                    await formattedResultSqlite.saveTestImages(reportPath, workers);
+                    return reportBuilderSqlite.addSuccess(formattedResultSqlite);
+                }).catch(reject));
+            }
+
+
         });
 
         hermione.on(hermione.events.RETRY, testResult => {
             promises.push(queue.add(() => failHandler(testResult).then(addFail)).catch(reject));
+            if (reportBuilderSqlite) {
+                promises.push(queue.add(() => failHandlerSqlite(testResult).then(addFailSqlite)).catch(reject));
+            }
+
         });
 
         hermione.on(hermione.events.TEST_FAIL, testResult => {
             promises.push(queue.add(() => failHandler(testResult).then(addFail)).catch(reject));
+            if (reportBuilderSqlite) {
+                promises.push(queue.add(() => failHandlerSqlite(testResult).then(addFailSqlite)).catch(reject));
+            }
         });
 
         hermione.on(hermione.events.TEST_PENDING, testResult => {
             promises.push(queue.add(() => failHandler(testResult).then((testResult) => reportBuilder.addSkipped(testResult)).catch(reject)));
+            if (reportBuilderSqlite) {
+                promises.push(queue.add(() => failHandlerSqlite(testResult).then((testResult) => reportBuilderSqlite.addSkipped(testResult)).catch(reject)));
+            }
         });
 
         hermione.on(hermione.events.RUNNER_END, (stats) => {
             reportBuilder
                 .setStats(stats)
                 .setApiValues(hermione.htmlReporter.values);
+
+            if (reportBuilderSqlite) {
+                reportBuilderSqlite
+                    .setStats(stats)
+                    .setApiValues(hermione.htmlReporter.values);
+            }
+
 
             return Promise.all(promises).then(resolve, reject);
         });
