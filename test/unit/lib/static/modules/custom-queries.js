1d833223ee8c40e68d2369ccc9bfcffcfeddca55
rmdm <rmdm@yandex-team.ru>
2019-12-20 15:56:40 +0300

    feat: allow to share specific test

---
+++ test/unit/lib/static/modules/custom-queries.js
@@ -0,0 +1,34 @@
+'use strict';
+import {getViewQuery} from 'lib/static/modules/custom-queries';
+
+describe('lib/static/modules/query-params', () => {
+    describe('getViewQuery', () => {
+        it('returns empty query object when no query params exist', () => {
+            const query = getViewQuery('');
+
+            assert.deepStrictEqual(query, {});
+        });
+
+        it('parses specified view-related values', () => {
+            const query = getViewQuery([
+                'browser=safari%20browser',
+                'browser=firefox',
+                'testNameFilter=test name',
+                'retryIndex=10',
+                'viewMode=all',
+                'expand=none',
+                'groupByError=true',
+                'abc=111'
+            ].join('&'));
+
+            assert.deepStrictEqual(query, {
+                filteredBrowsers: ['safari browser', 'firefox'],
+                testNameFilter: 'test name',
+                retryIndex: 10,
+                viewMode: 'all',
+                expand: 'none',
+                groupByError: true
+            });
+        });
+    });
+});
