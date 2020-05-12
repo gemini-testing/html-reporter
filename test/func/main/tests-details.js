213a7e2cb859c95c855fbbe5391c10edc0972839
DudaGod <dudkevich@yandex-team.ru>
2020-05-12 14:32:00 +0300

    feat(style): implement the main frame of the new design
    BREAKING CHANGE: report design has completely changed

--- test/func/main/tests-details.js
+++ test/func/main/tests-details.js
@@ -15,7 +15,7 @@ describe('Test details', function() {
     it('should prevent details summary overflow', function() {
         const selector = mkNestedSelector(
             '.section[title="test with long error message"]',
-            '.error .details__summary'
+            '.error .details'
         );
 
         return this.browser
