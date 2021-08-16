e728fcd29aebe49f49f3bef2a8de65b995758227
Vladislav Novikov <mb1te.comics@gmail.com>
2021-08-16 18:45:27 +0300

    feat: add query params support to metaInfoBaseUrls

--- test/unit/lib/static/components/section/body/meta-info.js
+++ test/unit/lib/static/components/section/body/meta-info.js
@@ -103,25 +103,71 @@ describe('<MetaInfo />', () => {
     [
         {
             type: 'path',
+            metaInfo: {
+                file: 'test/file'
+            },
             metaInfoBaseUrls: {file: 'base/path'},
             expectedFileUrl: 'base/path/test/file'
         },
         {
             type: 'url',
+            metaInfo: {
+                file: 'test/file'
+            },
             metaInfoBaseUrls: {file: 'http://127.0.0.1'},
             expectedFileUrl: 'http://127.0.0.1/test/file'
+        },
+        {
+            type: 'url with one query param',
+            metaInfo: {
+                file: 'test/file'
+            },
+            metaInfoBaseUrls: {
+                file: 'http://127.0.0.1?a=b'
+            },
+            expectedFileUrl: 'http://127.0.0.1/test/file?a=b'
+        },
+        {
+            type: 'url with few query params',
+            metaInfo: {
+                file: 'test/file'
+            },
+            metaInfoBaseUrls: {
+                file: 'http://127.0.0.1?a=b&b=c&c=d'
+            },
+            expectedFileUrl: 'http://127.0.0.1/test/file?a=b&b=c&c=d'
+        },
+        {
+            type: 'url with pathname',
+            metaInfo: {
+                file: 'test/file'
+            },
+            metaInfoBaseUrls: {
+                file: 'http://127.0.0.1/path/'
+            },
+            expectedFileUrl: 'http://127.0.0.1/path/test/file'
+        },
+        {
+            type: 'url with query params and meta value with query params',
+            metaInfo: {
+                file: 'test/file?a=b'
+            },
+            metaInfoBaseUrls: {
+                file: 'http://127.0.0.1/?c=d&e=f'
+            },
+            expectedFileUrl: 'http://127.0.0.1/test/file?a=b&c=d&e=f'
         }
     ].forEach((stub) => {
         it(`should render link in meta info based upon metaInfoBaseUrls ${stub.type}`, () => {
-            const result = {
-                metaInfo: {file: 'test/file'}
-            };
             const initialConfig = {config: {metaInfoBaseUrls: stub.metaInfoBaseUrls}};
-
+            const result = {
+                metaInfo: stub.metaInfo
+            };
             const component = mkMetaInfoComponent({result}, initialConfig);
+            component.debug();
             component.find('.details__summary').simulate('click');
 
-            assert.equal(component.find('.meta-info__item:first-child').text(), 'file: test/file');
+            assert.equal(component.find('.meta-info__item:first-child').text(), `file: ${stub.metaInfo.file}`);
             assert.equal(component.find('.meta-info__item:first-child a').prop('href'), stub.expectedFileUrl);
         });
     });
