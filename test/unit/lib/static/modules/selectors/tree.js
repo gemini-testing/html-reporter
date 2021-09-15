9ac300de7a0c6525532de1d2025f4588a2575a96
DudaGod <dudkevich@yandex-team.ru>
2021-09-15 15:42:44 +0300

    fix(gui): incorrect tests counting
    - incorrect counting of the number of images on click "Accept opened"
    - incorrect counting of the number of errors in the "Group by error" mode
    - incorrect counting of the number of tests to run by button "Retry failed tests"

--- test/unit/lib/static/modules/selectors/tree.js
+++ test/unit/lib/static/modules/selectors/tree.js
@@ -1,9 +1,11 @@
 import {SUCCESS, FAIL, IDLE} from 'lib/constants/test-statuses';
 import {
     getAcceptableImagesByStateName,
+    getAcceptableOpenedImageIds,
     mkGetLastImageByStateName,
     areAllRootSuitesIdle,
-    getFailedTests
+    getFailedTests,
+    getFailedSuiteResults
 } from 'lib/static/modules/selectors/tree';
 import {mkSuite, mkBrowser, mkResult, mkImage, mkStateTree, mkStateView} from '../../state-utils';
 
@@ -120,6 +122,125 @@ describe('tree selectors', () => {
         });
     });
 
+    describe('"getAcceptableOpenedImageIds" selector', () => {
+        describe('should not return images if', () => {
+            const _mkStateTree = () => {
+                const suitesById = {...mkSuite({id: 's1', browserIds: ['b1']})};
+                const suitesStateById = {s1: {shouldBeShown: true, shouldBeOpened: true}};
+                const suitesAllRootIds = ['s1'];
+
+                const browsersById = {...mkBrowser({id: 'b1', resultIds: ['r1']})};
+                const browsersStateById = {b1: {shouldBeShown: true, shouldBeOpened: true, retryIndex: 0}};
+
+                const resultsById = {...mkResult({id: 'r1', parentId: 'b1', imageIds: ['img1']})};
+
+                const imagesById = {...mkImage({id: 'img1', parentId: 'r1', stateName: 'first', status: FAIL})};
+                const imagesStateById = {img1: {shouldBeOpened: true}};
+
+                return mkStateTree({
+                    suitesById, suitesStateById, suitesAllRootIds, browsersById, browsersStateById,
+                    resultsById, imagesById, imagesStateById
+                });
+            };
+
+            it('suite is not shown', () => {
+                const tree = _mkStateTree();
+                tree.suites.stateById['s1'].shouldBeShown = false;
+
+                const state = mkState({tree});
+
+                assert.deepEqual(getAcceptableOpenedImageIds(state), []);
+            });
+
+            it('suite is not opened', () => {
+                const tree = _mkStateTree();
+                tree.suites.stateById['s1'].shouldBeOpened = false;
+
+                const state = mkState({tree});
+
+                assert.deepEqual(getAcceptableOpenedImageIds(state), []);
+            });
+
+            it('browser is not shown', () => {
+                const tree = _mkStateTree();
+                tree.browsers.stateById['b1'].shouldBeShown = false;
+
+                const state = mkState({tree});
+
+                assert.deepEqual(getAcceptableOpenedImageIds(state), []);
+            });
+
+            it('browser is not opened', () => {
+                const tree = _mkStateTree();
+                tree.browsers.stateById['b1'].shouldBeOpened = false;
+
+                const state = mkState({tree});
+
+                assert.deepEqual(getAcceptableOpenedImageIds(state), []);
+            });
+
+            it('image is not opened', () => {
+                const tree = _mkStateTree();
+                tree.images.stateById['img1'].shouldBeOpened = false;
+
+                const state = mkState({tree});
+
+                assert.deepEqual(getAcceptableOpenedImageIds(state), []);
+            });
+
+            it('image is not acceptable', () => {
+                const tree = _mkStateTree();
+                tree.images.byId['img1'].status = SUCCESS;
+
+                const state = mkState({tree});
+
+                assert.deepEqual(getAcceptableOpenedImageIds(state), []);
+            });
+        });
+
+        it('should return opened images', () => {
+            const suitesById = {
+                ...mkSuite({id: 's1', suiteIds: ['s2'], browserIds: ['b1']}),
+                ...mkSuite({id: 's2', browserIds: ['b2']})
+            };
+            const suitesStateById = {
+                s1: {shouldBeShown: true, shouldBeOpened: true},
+                s2: {shouldBeShown: true, shouldBeOpened: true}
+            };
+            const suitesAllRootIds = ['s1'];
+
+            const browsersById = {
+                ...mkBrowser({id: 'b1', resultIds: ['r1']}),
+                ...mkBrowser({id: 'b2', resultIds: ['r2']})
+            };
+            const browsersStateById = {
+                b1: {shouldBeShown: true, shouldBeOpened: true, retryIndex: 0},
+                b2: {shouldBeShown: true, shouldBeOpened: true, retryIndex: 0}
+            };
+
+            const resultsById = {
+                ...mkResult({id: 'r1', parentId: 'b1', imageIds: ['img1']}),
+                ...mkResult({id: 'r2', parentId: 'b2', imageIds: ['img2']})
+            };
+            const imagesById = {
+                ...mkImage({id: 'img1', parentId: 'r1', stateName: 'first', status: FAIL}),
+                ...mkImage({id: 'img2', parentId: 'r2', stateName: 'second', status: FAIL})
+            };
+            const imagesStateById = {img1: {shouldBeOpened: true}, img2: {shouldBeOpened: true}};
+
+            const tree = mkStateTree({
+                suitesById, suitesStateById, suitesAllRootIds, browsersById, browsersStateById,
+                resultsById, imagesById, imagesStateById
+            });
+            const state = mkState({tree});
+
+            assert.deepEqual(
+                getAcceptableOpenedImageIds(state),
+                ['img1', 'img2']
+            );
+        });
+    });
+
     describe('"mkGetLastImageByStateName" factory', () => {
         it('should return image with passed "id" if there are no images with the same state name', () => {
             const browsersById = mkBrowser({id: 'b', resultIds: ['r1', 'r2']});
@@ -185,13 +306,23 @@ describe('tree selectors', () => {
 
     describe('"getFailedTests" selector', () => {
         it('should return failed tests with metaInfo', () => {
-            const suitesById = mkSuite({id: 's1', status: FAIL, browserIds: ['b']});
-            const browsersById = mkBrowser({id: 'b', name: 'browser', parentId: 'test', resultIds: ['r1']});
-            const resultsById = mkResult({id: 'r1', parentId: 'b', status: FAIL, metaInfo: {param: 'value'}});
+            const suitesById = {
+                ...mkSuite({id: 's1', suiteIds: ['s2'], browserIds: ['b1'], status: FAIL}),
+                ...mkSuite({id: 's2', browserIds: ['b2'], status: FAIL})
+            };
+            const suitesFailedRootIds = ['s1'];
+            const browsersById = {
+                ...mkBrowser({id: 'b1', name: 'browser1', parentId: 's1', resultIds: ['r1']}),
+                ...mkBrowser({id: 'b2', name: 'browser2', parentId: 's2', resultIds: ['r2']})
+            };
+            const resultsById = {
+                ...mkResult({id: 'r1', parentId: 'b1', status: FAIL, metaInfo: {param: 'value1'}}),
+                ...mkResult({id: 'r2', parentId: 'b2', status: FAIL, metaInfo: {param: 'value2'}})
+            };
 
             const tree = mkStateTree({
-                suitesFailedRootIds: ['s1'],
                 suitesById,
+                suitesFailedRootIds,
                 browsersById,
                 resultsById
             });
@@ -199,11 +330,51 @@ describe('tree selectors', () => {
 
             assert.deepEqual(
                 getFailedTests(state),
-                [{
-                    'browserName': 'browser',
-                    'metaInfo': {param: 'value'},
-                    'testName': 'test'
-                }]
+                [
+                    {
+                        'browserName': 'browser1',
+                        'metaInfo': {param: 'value1'},
+                        'testName': 's1'
+                    },
+                    {
+                        'browserName': 'browser2',
+                        'metaInfo': {param: 'value2'},
+                        'testName': 's2'
+                    }
+                ]
+            );
+        });
+    });
+
+    describe('"getFailedSuiteResults" helper', () => {
+        it('should return all failed results', () => {
+            const suitesById = {
+                ...mkSuite({id: 's1', suiteIds: ['s2'], browserIds: ['b1'], status: FAIL}),
+                ...mkSuite({id: 's2', browserIds: ['b2'], status: FAIL}),
+                ...mkSuite({id: 's3', browserIds: ['b3'], status: SUCCESS})
+            };
+            const suitesFailedRootIds = ['s1'];
+            const browsersById = {
+                ...mkBrowser({id: 'b1', resultIds: ['r1']}),
+                ...mkBrowser({id: 'b2', resultIds: ['r2']}),
+                ...mkBrowser({id: 'b3', resultIds: ['r3']})
+            };
+            const resultsById = {
+                ...mkResult({id: 'r1', parentId: 'b1'}),
+                ...mkResult({id: 'r2', parentId: 'b2'}),
+                ...mkResult({id: 'r3', parentId: 'b3'})
+            };
+
+            const tree = mkStateTree({
+                suitesById,
+                suitesFailedRootIds,
+                browsersById,
+                resultsById
+            });
+
+            assert.deepEqual(
+                getFailedSuiteResults(tree),
+                [resultsById.r1, resultsById.r2]
             );
         });
     });
