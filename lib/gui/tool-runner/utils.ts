36a38e03ac652c6c1a43b7354a4006308b076190
shadowusr <nikhakerlab@gmail.com>
2023-11-08 00:13:07 +0300

    chore: rewrite tool-runner/utils to typescript

--- lib/gui/tool-runner/utils.ts
+++ lib/gui/tool-runner/utils.ts
@@ -1,26 +1,23 @@
-'use strict';
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import Database from 'better-sqlite3';
+import type {CoordBounds} from 'looks-same';
 
-const _ = require('lodash');
-const path = require('path');
-const fs = require('fs-extra');
-const chalk = require('chalk');
-const Database = require('better-sqlite3');
+import {logger} from '../../common-utils';
+import {DATABASE_URLS_JSON_NAME, LOCAL_DATABASE_NAME} from '../../constants';
+import {mergeTables} from '../../db-utils/server';
+import {TestEqualDiffsData, TestRefUpdateData} from '../../tests-tree-builder/gui';
+import {ImageInfoFail, ImageSize} from '../../types';
 
-const {logger} = require('../../common-utils');
-const {DATABASE_URLS_JSON_NAME, LOCAL_DATABASE_NAME} = require('../../constants/database');
-const {mergeTables} = require('../../db-utils/server');
+export const formatId = (hash: string, browserId: string): string => `${hash}/${browserId}`;
 
-exports.formatId = (hash, browserId) => `${hash}/${browserId}`;
-
-exports.mkFullTitle = ({suite, state}) => {
-    return suite.path.length > 0
-        ? `${suite.path.join(' ')} ${state.name}`
-        : state.name;
+export const mkFullTitle = ({suite, state}: Pick<TestRefUpdateData, 'suite' | 'state'>): string => {
+    return suite.path.length > 0 ? `${suite.path.join(' ')} ${state.name}` : state.name;
 };
 
-// 'databaseUrls.json' may contain many databases urls but html-reporter at gui mode can work with single databases file.
-// all databases should be local files.
-exports.mergeDatabasesForReuse = async (reportPath) => {
+export const mergeDatabasesForReuse = async (reportPath: string): Promise<void> => {
     const dbUrlsJsonPath = path.resolve(reportPath, DATABASE_URLS_JSON_NAME);
     const mergedDbPath = path.resolve(reportPath, LOCAL_DATABASE_NAME);
 
@@ -28,7 +25,7 @@ exports.mergeDatabasesForReuse = async (reportPath) => {
         return;
     }
 
-    const {dbUrls = []} = await fs.readJson(dbUrlsJsonPath);
+    const {dbUrls = []}: {dbUrls: string[]} = await fs.readJson(dbUrlsJsonPath);
 
     const dbPaths = dbUrls
         .filter(u => u !== LOCAL_DATABASE_NAME)
@@ -41,15 +38,19 @@ exports.mergeDatabasesForReuse = async (reportPath) => {
     logger.warn(chalk.yellow(`Merge databases to ${LOCAL_DATABASE_NAME}`));
 
     const mergedDatabase = new Database(mergedDbPath);
-    mergeTables({db: mergedDatabase, dbPaths, getExistingTables: (statement) => {
-        return statement.all().map((table) => table.name);
-    }});
+    mergeTables({
+        db: mergedDatabase,
+        dbPaths,
+        getExistingTables: (statement: Database.Statement) => {
+            return statement.all().map((table) => (table as {name: string}).name);
+        }
+    });
     mergedDatabase.close();
 
     await Promise.all(dbPaths.map(p => fs.remove(p)));
 };
 
-exports.filterByEqualDiffSizes = (imagesInfo, refDiffClusters) => {
+export const filterByEqualDiffSizes = (imagesInfo: TestEqualDiffsData[], refDiffClusters: CoordBounds[]): TestEqualDiffsData[] => {
     if (_.isEmpty(refDiffClusters)) {
         return [];
     }
@@ -57,7 +58,10 @@ exports.filterByEqualDiffSizes = (imagesInfo, refDiffClusters) => {
     const refDiffSizes = refDiffClusters.map(getDiffClusterSizes);
 
     return _.filter(imagesInfo, (imageInfo) => {
-        const imageDiffSizes = imageInfo.diffClusters.map(getDiffClusterSizes);
+        // TODO: get rid of type assertion here
+        const imageInfoFail = imageInfo as ImageInfoFail;
+
+        const imageDiffSizes = imageInfoFail.diffClusters?.map(getDiffClusterSizes) ?? [];
         const equal = compareDiffSizes(imageDiffSizes, refDiffSizes);
 
         if (!equal) {
@@ -65,21 +69,21 @@ exports.filterByEqualDiffSizes = (imagesInfo, refDiffClusters) => {
         }
 
         if (!_.isEqual(imageDiffSizes, refDiffSizes)) {
-            imageInfo.diffClusters = reorderClustersByEqualSize(imageInfo.diffClusters, imageDiffSizes, refDiffSizes);
+            imageInfoFail.diffClusters = reorderClustersByEqualSize(imageInfoFail.diffClusters ?? [], imageDiffSizes, refDiffSizes);
         }
 
         return true;
     });
 };
 
-function getDiffClusterSizes(diffCluster) {
+function getDiffClusterSizes(diffCluster: CoordBounds): ImageSize {
     return {
         width: diffCluster.right - diffCluster.left + 1,
         height: diffCluster.bottom - diffCluster.top + 1
     };
 }
 
-function compareDiffSizes(diffSizes1, diffSizes2) {
+function compareDiffSizes(diffSizes1: ImageSize[], diffSizes2: ImageSize[]): boolean {
     if (diffSizes1.length !== diffSizes2.length) {
         return false;
     }
@@ -91,22 +95,22 @@ function compareDiffSizes(diffSizes1, diffSizes2) {
             return false;
         }
 
-        diffSizes2 = diffSizes2.filter((v, ind) => ind !== foundIndex);
+        diffSizes2 = diffSizes2.filter((_v, ind) => ind !== foundIndex);
 
         return true;
     });
 }
 
-function reorderClustersByEqualSize(diffClusters1, diffSizes1, diffSizes2) {
+function reorderClustersByEqualSize(diffClusters1: CoordBounds[], diffSizes1: ImageSize[], diffSizes2: ImageSize[]): CoordBounds[] {
     return diffClusters1.reduce((acc, cluster, i) => {
         if (diffSizes1[i] !== diffSizes2[i]) {
             const foundIndex = _.findIndex(diffSizes2, diffSizes1[i]);
-            diffSizes2 = diffSizes2.filter((v, ind) => ind !== foundIndex);
+            diffSizes2 = diffSizes2.filter((_v, ind) => ind !== foundIndex);
             acc[foundIndex] = cluster;
         } else {
             acc[i] = cluster;
         }
 
         return acc;
-    }, []);
+    }, [] as CoordBounds[]);
 }
