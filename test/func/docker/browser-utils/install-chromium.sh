f028f037a039c3ad9001d0a768c93e750a349707
shadowusr <nikhakerlab@gmail.com>
2023-12-12 03:40:21 +0300

    test: update chrome installation flow

--- test/func/docker/browser-utils/install-chromium.sh
+++ test/func/docker/browser-utils/install-chromium.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# The download script outputs progress messages, but we need only final path, hence we filter output here
-CHROME_PATH=$(node ./download-chromium.js |& grep '^/home')
-CHROME_PATH=$(dirname $CHROME_PATH)
+CHROME_PATH=$(node ./download-chromium.js)
 mkdir ~/browsers
 mv $CHROME_PATH ~/browsers/chrome-linux
