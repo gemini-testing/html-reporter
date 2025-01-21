5197f815401c3c6288c661e01ce45a3c15d26034
= <=>
2025-01-21 12:37:26 +0300

    feat(error-handler): change data corruption fallback style

--- lib/static/new-ui/features/error-handling/components/ErrorHandling/fallbacks.tsx
+++ lib/static/new-ui/features/error-handling/components/ErrorHandling/fallbacks.tsx
@@ -66,7 +66,7 @@ export function FallbackCardCrash({limited}: FallbackProps): ReactNode {
 export function FallbackDataCorruption({limited}: FallbackProps): ReactNode {
     const {state} = useErrorContext();
 
-    return <div className={classNames(styles.crash, {[styles.limited]: limited})}>
+    return <div className={classNames(styles.crash, styles.dense, {[styles.limited]: limited})}>
         <Text variant="body-1" color="secondary">The data is corrupted or there’s a bug on our side. <Link href={NEW_ISSUE_LINK} target='_blank'>File an issue</Link></Text>
 
         <ErrorInfoFc className={styles.errorInfo} name={state.error.name} stack={state.error.stack} />
