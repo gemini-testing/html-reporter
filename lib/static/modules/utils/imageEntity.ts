7f4a6655187673c5c6e78d234d6e4b978fd0cb03
= <=>
2025-01-21 09:59:55 +0300

    refactor(prefetch): global link cache added

--- lib/static/modules/utils/imageEntity.ts
+++ lib/static/modules/utils/imageEntity.ts
@@ -1,17 +1,40 @@
 import {ImageEntity, ImageEntityCommitted, ImageEntityError, ImageEntityFail, ImageEntityStaged, ImageEntitySuccess, ImageEntityUpdated} from '../../new-ui/types/store';
 
+class LinkCache {
+    private static cache: Record<string, HTMLLinkElement | undefined> = {};
+
+    private static createPreloadLink(url: string): HTMLLinkElement {
+        const link = document.createElement('link');
+
+        link.rel = 'preload';
+        link.as = 'image';
+        link.href = url;
+
+        document.head.appendChild(link);
+
+        return link;
+    }
+
+    static register(url: string): void {
+        if (LinkCache.cache[url]) {
+            return;
+        }
+
+        LinkCache.cache[url] = LinkCache.createPreloadLink(url);
+    }
+
+    static dispose(url: string): void {
+        LinkCache.cache[url]?.remove();
+
+        delete LinkCache.cache[url];
+    }
+}
+
 // TODO: remove export when old ui is removed
-export function preloadImage(url: string): HTMLElement {
-    const link = document.createElement('link');
+export function preloadImage(url: string): () => void {
+    LinkCache.register(url);
 
-    link.rel = 'preload';
-    link.as = 'image';
-    link.href = url;
-    link.onload;
-
-    document.head.appendChild(link);
-
-    return link;
+    return () => LinkCache.dispose(url);
 }
 
 function hasExpectedImage(image: ImageEntity): image is ImageEntityFail | ImageEntitySuccess | ImageEntityUpdated {
@@ -31,25 +54,23 @@ function hasRefImage(image: ImageEntity): image is ImageEntityFail {
 }
 
 export function preloadImageEntity(image: ImageEntity): () => void {
-    const elements: HTMLElement[] = [];
+    const disposeCallbacks: (() => void)[] = [];
 
     if (hasExpectedImage(image)) {
-        elements.push(preloadImage(image.expectedImg.path));
+        disposeCallbacks.push(preloadImage(image.expectedImg.path));
     }
 
     if (hasActualImage(image)) {
-        elements.push(preloadImage(image.actualImg.path));
+        disposeCallbacks.push(preloadImage(image.actualImg.path));
     }
 
     if (hasDiffImage(image)) {
-        elements.push(preloadImage(image.diffImg.path));
+        disposeCallbacks.push(preloadImage(image.diffImg.path));
     }
 
     if (hasRefImage(image)) {
-        elements.push(preloadImage(image.refImg.path));
+        disposeCallbacks.push(preloadImage(image.refImg.path));
     }
 
-    return (): void => {
-        elements.forEach(element => element.remove());
-    };
+    return () => disposeCallbacks.forEach(dispose => dispose());
 }
