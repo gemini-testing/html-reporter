4a77a126902b9cb0d9fcb38adb7e1386fdb32dfd
shadowusr <nikhakerlab@gmail.com>
2025-05-19 18:12:32 +0300

    feat: allow disabling config overrides for specific browsers

--- lib/gui/config-overrides.ts
+++ lib/gui/config-overrides.ts
@@ -23,14 +23,32 @@ export const overrideTestplaneConfig = (toolName: ToolName, config: TestplaneCon
         return;
     }
 
+    const skipBrowsers = ((): Set<string> => {
+        const envValue = process.env.HTML_REPORTER_NO_CONFIG_OVERRIDES_FOR_BROWSERS;
+        if (!envValue) {
+            return new Set<string>();
+        }
+
+        try {
+            return new Set(JSON.parse(envValue).map((s: string) => s.trim().toLowerCase()));
+        } catch {
+            return new Set(envValue.split(',').map(s => s.trim().toLowerCase()));
+        }
+    })();
+
     for (const browserId of config.browserIds) {
         const browserConfig = config.getBrowserConfig(browserId);
+        const browserName = browserConfig.desiredCapabilities?.browserName?.toLowerCase();
 
-        const browserName = browserConfig.desiredCapabilities?.browserName;
         debug(`Applying config overrides for browser: ${browserName}`);
 
-        if (browserName && /internet explorer/i.test(browserName)) {
+        if (browserName === 'internet explorer') {
             debug(`Skipping Internet Explorer`);
+            continue;
+        }
+
+        if (browserName && skipBrowsers.has(browserName) || skipBrowsers.has(browserId.toLowerCase())) {
+            debug(`Skipping overrides for browser: ${browserName} (browserId: ${browserId}) as it's in skip list`);
             continue;
         }
 
