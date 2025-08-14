de3cd3d2cab437a8b7d7728ca771fbc9c7b2520f
rocketraccoon <rocketraccoon@yandex-team.ru>
2025-08-14 13:17:37 +0700

    feat(new-ui): show screenshots from retry with errors #10

--- lib/static/new-ui/features/visual-checks/selectors.ts
+++ lib/static/new-ui/features/visual-checks/selectors.ts
@@ -59,17 +59,13 @@ export const getNamedImages = createSelector(
             const browser = browsers[group.browserId];
             const lastResultId = browser.resultIds[browser.resultIds.length - 1];
             const lastResult = results[lastResultId];
-            const hasImage = browser.resultIds.some((resultId) => (
-                results[resultId].imageIds.length > 0
-            ));
 
             // if last result 'ok' (not error and running) and doesn't have images we skip it
             // but if last result error (or running) we show it, because there are can be image
             if (
                 lastResult.status !== TestStatus.RUNNING &&
                 lastResult.status !== TestStatus.ERROR &&
-                !lastResult.imageIds.find(imageId => images[imageId].stateName === group.stateName) &&
-                !hasImage
+                !lastResult.imageIds.find(imageId => images[imageId].stateName === group.stateName)
             ) {
                 continue;
             }
