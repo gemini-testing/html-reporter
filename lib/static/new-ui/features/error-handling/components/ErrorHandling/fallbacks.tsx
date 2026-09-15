dd6b4bf1a20166dc629a80985a73c570ce630022
shadowusr <nikhakerlab@gmail.com>
2026-09-15 15:37:24 +0300

    fix: additional fixes of dark theme and animations

--- lib/static/new-ui/features/error-handling/components/ErrorHandling/fallbacks.tsx
+++ lib/static/new-ui/features/error-handling/components/ErrorHandling/fallbacks.tsx
@@ -1,20 +1,20 @@
 import {Divider, Link, Text} from '@gravity-ui/uikit';
 import classNames from 'classnames';
 import React, {ReactNode} from 'react';
-import TestplaneIcon from '../../../../../icons/testplane-mono-black.svg';
-import ExclamationTriangleLarge from '../../../../../icons/exclamation-triangle-large.svg';
 import {ErrorInfo as ErrorInfoFc} from '../../../../components/ErrorInfo';
 import styles from './index.module.css';
 import {useErrorContext} from './context';
 import {FileIssue, ReloadPage} from './actions';
 import {NEW_ISSUE_LINK} from '@/constants';
+import {TestplaneLogo} from '@/static/new-ui/components/MainLayout/logo';
+import {ExclamationTriangleLarge} from './exclamation-triangle';
 
 export function FallbackAppCrash(): ReactNode {
     const {state} = useErrorContext();
 
     return <div className={styles.crashAbsoluteWrapper}>
         <div className={classNames(styles.crash, styles.lined)}>
-            <img src={TestplaneIcon} alt="icon" width={32} height={32}/>
+            <TestplaneLogo width={32} height={32} />
 
             <div className={styles.divider} />
 
@@ -49,7 +49,7 @@ export function FallbackCardCrash({recommendedAction}: FallbackCardCrashProps): 
     const {state} = useErrorContext();
 
     return <div className={classNames(styles.crash)}>
-        <img src={ExclamationTriangleLarge} alt="icon" width={32} height={32} />
+        <ExclamationTriangleLarge width={32} height={32} />
 
         <Text variant="subheader-3">Something went wrong</Text>
         <Text variant="body-1" color="secondary">The data is corrupted or there’s a bug on our side</Text>
